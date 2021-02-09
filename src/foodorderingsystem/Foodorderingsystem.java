8a0917872f4cd01dfa8f2e0db59566957eb8b984
Kangajan <Kangajan@DESKTOP-GVE769R>
2021-02-09 18:28:47 +0530

    hello

---
+++ src/foodorderingsystem/Foodorderingsystem.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foodorderingsystem;
+
+/**
+ *
+ * @author Kangajan
+ */
+public class Foodorderingsystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        applicationform_foodorderingsystem af = new applicationform_foodorderingsystem();
+        af.setVisible(true);
+    }
+    
+}
